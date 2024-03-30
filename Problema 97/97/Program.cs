f27d8ec45df6d4f7910025ddb468ab4e0c2f6d75
jeanc0701 <jeancgw07@gmail.com>
2024-03-29 23:42:33 -0400

    Pseudocodigo del problema 97

---
+++ Problema 97/97/Program.cs
@@ -0,0 +1,15 @@
+﻿string Parity(List<int> a)
+{
+    string B= "";
+    for (int i = 0; i < a.Count(); i++)
+    {
+        if (a % 2 == 0)
+        {
+            B+="P"+" "
+        }else
+        {
+            B+="I"+" "
+        }
+    }
+    return B;
+}
